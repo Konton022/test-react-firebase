51d3480f3f2e0034ddffd5e5e6677877eefcfea3
Konton022 <42418105+Konton022@users.noreply.github.com>
2021-12-04 01:29:45 +0500

    add some bags

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import "./styles.css";
+import { useState, useEffect } from "react";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, getDocs } from "firebase/firestore";
 
 const firebaseApp = initializeApp({
   apiKey: "AIzaSyDxnqdfTPn0GaW5fBkRdSTnRy6jhJ7RRdw",
-
   authDomain: "test-reactapp-firebase.firebaseapp.com",
-
   projectId: "test-reactapp-firebase",
 });
 
 const db = getFirestore();
 
 export default function App() {
-  async function fetchFire() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [lastName, setLastName] = useState("");
+  async function fetchFire(event) {
+    event.preventDefault();
     try {
       const docRef = await addDoc(collection(db, "users"), {
-        first: "Ada",
-        last: "Lovelace",
-        born: 1815,
+        name,
+        lastName,
+        age,
       });
       console.log("Document written with ID: ", docRef.id);
     } catch (e) {
@@ -27,9 +30,40 @@ export default function App() {
     }
   }
 
+  async function getFire() {
+    const querySnapshot = await getDocs(collection(db, "users"));
+    console.log(querySnapshot);
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data()}`);
+    });
+    return querySnapshot;
+  }
+
+  // useEffect(async () => {
+  //   await getFire();
+  // }, []);
+
   return (
-    <div className="App">
-      <button onClick={fetchFire}>SEND</button>
+    <div className="App" onSubmit={fetchFire}>
+      <form>
+        <input
+          value={name}
+          placeholder="enter name"
+          onChange={(event) => setName(event.target.value)}
+        />
+        <input
+          value={lastName}
+          placeholder="enter last name"
+          onChange={(event) => setLastName(event.target.value)}
+        />
+        <input
+          value={age}
+          placeholder="enter your age"
+          onChange={(event) => setAge(event.target.value)}
+        />
+        <button type="submit">SEND</button>
+      </form>
+      <button onClick={getFire}> get DATA</button>
     </div>
   );
 }
